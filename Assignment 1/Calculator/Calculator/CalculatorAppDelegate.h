ffdb9fe0caa16d8fa6a28cf793dc41d95022fcf0
Michael Campbell <Mykomic87@gmail.com>
2013-02-16 16:38:42 -0500

    Adding Assignment 1

---
+++ Assignment 1/Calculator/Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Michael Campbell on 12/9/12.
+//  Copyright (c) 2012 Michael Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
