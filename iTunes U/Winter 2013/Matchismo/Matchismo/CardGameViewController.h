49320c1c3434db71aed24d9e16213ac8bbf0ff7f
Michael Campbell <Mykomic87@gmail.com>
2013-02-17 16:56:52 -0500

    Set up Matchismo project

---
+++ iTunes U/Winter 2013/Matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Michael Campbell on 2/17/13.
+//  Copyright (c) 2013 Michael Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
