c8a649fee4a8c76c1c5e861bcfed48136b702b49
Michael Campbell <Mykomic87@gmail.com>
2013-11-12 19:09:08 -0500

    Initial Commit

---
+++ Pace/basicsample/basicsample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  basicsample
+//
+//  Created by Michael Campbell on 11/12/13.
+//  Copyright (c) 2013 Michael Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Pace/basicsample/basicsample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  basicsample
+//
+//  Created by Michael Campbell on 11/12/13.
+//  Copyright (c) 2013 Michael Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
