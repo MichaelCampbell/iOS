723e5e3c9fce514a541a733dba927c2ce6b4ab37
Michael Campbell <Mykomic87@gmail.com>
2013-11-14 18:16:18 -0500

    Initial Commit

---
+++ Pace/ObjCsample/ObjCsample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ObjCsample
+//
+//  Created by Michael Campbell on 11/14/13.
+//  Copyright (c) 2013 Michael Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Pace/ObjCsample/ObjCsampleTests/ObjCsampleTests.m
@@ -0,0 +1,34 @@
+//
+//  ObjCsampleTests.m
+//  ObjCsampleTests
+//
+//  Created by Michael Campbell on 11/14/13.
+//  Copyright (c) 2013 Michael Campbell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObjCsampleTests : XCTestCase
+
+@end
+
+@implementation ObjCsampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
