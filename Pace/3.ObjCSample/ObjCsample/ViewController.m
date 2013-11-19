aee47390f797fea460c86b84e1d4e39aaf3721f7
Michael Campbell <Mykomic87@gmail.com>
2013-11-19 17:57:04 -0500

    leftovers from end of class 4

--- Pace/3.ObjCSample/ObjCsample/ViewController.m
+++ Pace/3.ObjCSample/ObjCsample/ViewController.m
@@ -56,8 +56,108 @@
 
 - (void) looping
 {
+    /*
     for (int i = 0; i < 10; i++) {
         NSLog(@"Index: %i", i);
+    }
+    */
+    
+    /*
+    for (int i = 10; i > 0; i--) {
+        NSLog(@"Index: %i", i);
+    }
+    */
+    
+    /*
+    int i,j;
+    for (i = 0, j = 1; i < 10; i++, j*=2)
+    {
+        NSLog(@"I: %i, J: %i", i, j);
+        
+        if (i == 3)
+        {
+            break;
+        }
+    }
+    */
+    
+    /*
+    //do while
+    int i = 0;
+    do {
+        NSLog(@"%i", i);
+        i++;
+    } while (i < 10);
+    */
+    
+    //while
+    int i = 0;
+    while (i < 10) {
+        NSLog(@"%i", i);
+        i++;
+    }
+    
+}
+
+- (void) decisions
+{
+    //If
+    int i;
+    i = 10;
+    
+    //one line execution
+    /*
+    if (i==10)
+        NSLog(@"I is equal to 10");
+    else
+        NSLog(@"I is not equal to 10");
+    */
+    
+    //multiple line execution
+    /*
+    if (i == 10) {
+        NSLog(@"I is equal to 10");
+        i = i + 1;
+    } else {
+        NSLog(@"I is equal to 10");
+        i = i - 1;
+    }
+    */
+    
+    //muliple condition check
+    /*
+    if (i > 2 && i < 6) {
+        NSLog(@"I is between 2 and 6");
+        i++;
+    } else {
+        NSLog(@"I is not between 2 and 6");
+        i++;
+    }
+    */
+    
+    /*
+    if (i > 2 || i < 6) {
+        NSLog(@"I is either greater than 2 or less than 6");
+        i++;
+    } else {
+        NSLog(@"I is neither greater than 2 nor less than 6");
+        i++;
+    }
+    */
+    
+    //nested if...else
+    if (i == 2) {
+        NSLog(@"I is equal to 2");
+        i++;
+    } else if (i == 5) {
+        NSLog(@"I is equal to 5");
+        i++;
+    } else if (i == 6) {
+        NSLog(@"I is equal to 6");
+        i++;
+    } else {
+        NSLog(@"I is not equal to 3 or 5 or 6");
+        i++;
     }
     
 }
